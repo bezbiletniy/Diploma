e6efa1ae6c09502ae983c1c02fb406ba2bfa70f8
bezbiletniy <a.borodin.ca@gmail.com>
2022-10-25 13:57:28 +0300

    Add part1 SqlGelper

--- src/test/java/ru/netology/data/SqlHelper.java
+++ src/test/java/ru/netology/data/SqlHelper.java
@@ -1,7 +1,27 @@
 package ru.netology.data;
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class SqlHelper {
 
+    private static QueryRunner runner = new QueryRunner();
+    private SqlHelper() {
+    }
 
+    @SneakyThrows
+    private static Connection getConnect() {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/aqa-shop.jar", "app", "pass");
+    }
+
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var connection = getConnect();
+        runner.execute(connection, "DELETE FROM ???");
+        runner.execute(connection, "DELETE FROM ???");
+        runner.execute(connection, "DELETE FROM ???");
+        runner.execute(connection, "DELETE FROM ???");
+    }
 
 }
